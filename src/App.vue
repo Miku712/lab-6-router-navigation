a63420b6ef15e4a09123c1c4f9ba5aa43b2cd809
Nataliia <natalialysenko712@gmail.com>
2026-03-26 11:26:10 +0200

    Task 1: setup router + base routes

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>My App</h1>
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  padding: 40px;
+  text-align: center;
+}
+h1 {
+  color: #0369a1;
+  margin-bottom: 20px;
+}
+</style>
